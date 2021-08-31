de2e6541b4f6e63f76c8bcbb6b342716f8862ff7
Muniwedesu <mfmffmm@gmail.com>
2021-08-31 19:38:55 +0300

    removed unnecessary code, added top margin to post-details view

--- FrontAngularTest/src/app/post-data.service.ts
+++ FrontAngularTest/src/app/post-data.service.ts
@@ -14,18 +14,15 @@ export class PostDataService {
     return posts;
   }
   get(id) {
-    //receive a copy of the Post, not a link to array element
+    //receive copied Post obj
     const post = of({ ...POSTS.find(x => x.id === id) });
     return post;
   }
   create(post?: Post) {
-    console.log(`create ${post}`)
-    //POST
     const newPost = new Post(POSTS.length + 1, post.title, post.content);
     POSTS.push(newPost);
   }
   update(post: Post) {
-    console.log(`update ${post}`)
     //emulates sending new object to a server
     const postToUpdate = POSTS.find(p => p.id === post.id);
     //this should be done at backend
@@ -34,7 +31,6 @@ export class PostDataService {
     }
   }
   delete(id: number) {
-    console.log(`deleting post ${id}`);
     POSTS.splice(POSTS.findIndex((p: Post) => p.id === id), 1);
   }
 }
